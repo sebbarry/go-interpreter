3e28a34a10be1a650c50c7976bff685f3e7d8786
Sebastian Barry <dev@mmbsoft.com>
2022-03-28 21:52:53 -0400

    parser functionality

--- parser/parser.go
+++ parser/parser.go
@@ -1,9 +1,9 @@
 package parser
 
 import (
-    "interpreter/ast"
-    "interpreter/token"
-    "interpreter/lexer"
+	"interpreter/ast"
+	"interpreter/lexer"
+	"interpreter/token"
 )
 
 
@@ -32,7 +32,80 @@ func (p *Parser) nextToken() {
     p.peekToken = p.l.NextToken()
 }
 
+// parse the incoming stream
 func (p *Parser) ParseProgram() *ast.Program {
-    return nil
+
+    program := &ast.Program{}
+    program.Statements = []ast.Statement{} // slice of statements.
+
+    for p.curToken.Type != token.EOF {
+        stmt := p.parseStatement()
+        if stmt != nil {
+            // add to statement slice
+            program.Statements = append(program.Statements, stmt)
+        }
+        p.nextToken() // loop to the next token 
+        return nil
+    }
+    return program
 }
 
+
+/*
+switch between different types of statements.
+*/
+func (p *Parser) parseStatement() ast.Statement {
+    switch p.curToken.Type {
+    case token.LET:
+        return p.parseLetStatement()
+    default:
+        return nil
+    }
+}
+
+
+
+// let statement parser function
+func (p *Parser) parseLetStatement() *ast.LetStatement {
+    stmt := &ast.LetStatement{Token: p.curToken}
+
+    if !p.expectPeek(token.IDENT) {
+        return nil
+    }
+
+    stmt.Name = &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+
+    if !p.expectPeek(token.ASSIGN) {
+        return nil
+    }
+
+    // while the current token is not a ';'
+    for !p.curTokenIs(token.SEMICOLON) {
+        p.nextToken()
+    }
+
+    return stmt
+
+}
+
+
+
+func (p *Parser) curTokenIs(t token.TokenType) bool {
+    return p.curToken.Type == t
+}
+
+
+
+func (p *Parser) peekTokenIs(t token.TokenType) bool {
+    return p.peekToken.Type == t
+}
+
+// assertion function common to parsers.
+func (p *Parser) expectPeek(t token.TokenType) bool {
+    if p.peekTokenIs(t) {
+        p.nextToken()
+        return true
+    } else {
+        return false
+    }
+}
